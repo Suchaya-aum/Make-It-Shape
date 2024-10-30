9b68b55371b6f724d1245b76cf3bedb5a17bbafe
Suchayaa <74755774+Suchaya-aum@users.noreply.github.com>
2024-10-30 21:58:38 +0700

    Update Navbar.jsx

--- make_it_shape/src/app/components/Navbar.jsx
+++ make_it_shape/src/app/components/Navbar.jsx
@@ -6,11 +6,6 @@ function Navbar() {
     <nav className='bg-[#ffff] text-[#044b65] p-2 '>
       <div className="container p-5 mx-auto shadow-lg rounded-lg">
         <div className='flex justify-between items-center px-8'>
-            <div>
-                <Link href='/'>
-                  <span className="text-lg font-bold">Home</span>
-                </Link>
-            </div>
             <ul className='flex space-x-4 text-[#044b65] '>
                 <li><Link href='https://github.com/Suchaya-aum/Make-It-Shape'>Source Code</Link></li>
                 <li><Link href='https://suchaya-aum.github.io/Make-It-Shape/'>github.io</Link></li>
